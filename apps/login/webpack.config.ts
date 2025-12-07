cb277a4d13f0f322babe627268203c8cafc43859
Christian Lüdemann <christian@christianlydemann.com>
2025-12-07 12:22:20 +0100

    chore: rspack remote

--- apps/login/webpack.config.ts
+++ apps/login/webpack.config.ts
@@ -1,4 +1,17 @@
-import { withModuleFederation } from '@nx/module-federation/angular';
+import {
+  NxModuleFederationPlugin,
+  NxModuleFederationDevServerPlugin,
+} from '@nx/module-federation/angular';
 import config from './module-federation.config';
 
-export default withModuleFederation(config, { dts: false });
+export default {
+  plugins: [
+    new NxModuleFederationPlugin(
+      { config },
+      {
+        dts: false,
+      }
+    ),
+    new NxModuleFederationDevServerPlugin({ config }),
+  ],
+};
